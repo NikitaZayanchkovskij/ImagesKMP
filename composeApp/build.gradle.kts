b84fa01b2113a9f854ed7262e037985c92aeee14
Nikita Zayanchkovskij <zayanchkovskij.nikita@gmail.com>
2026-03-10 18:07:30 +0300

    Initial project set up

---
+++ composeApp/build.gradle.kts
@@ -0,0 +1,69 @@
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.composeMultiplatform)
+    alias(libs.plugins.composeCompiler)
+    alias(libs.plugins.androidKmpLibrary)
+    alias(libs.plugins.kotlin.serialization)
+}
+
+kotlin {
+    android {
+        namespace = "com.mikitazayanchkouski.imageskmp.composeapp"
+        compileSdk = libs.versions.android.compileSdk.get().toInt()
+        minSdk = libs.versions.android.minSdk.get().toInt()
+
+        /* Explicitly enabling Android resources, that are been generated,
+         * to be able to make our debug build run, on an Ios simulator.
+         */
+        experimentalProperties["android.experimental.kmp.enableAndroidResources"] = true
+    }
+    
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach { iosTarget ->
+        iosTarget.binaries.framework {
+            baseName = "ComposeApp"
+            isStatic = true
+        }
+    }
+    
+    sourceSets {
+        androidMain.dependencies {
+            implementation(libs.compose.ui.tooling.preview)
+            implementation(libs.androidx.activity.compose)
+
+            implementation(libs.ktor.client.okhttp)
+        }
+        commonMain.dependencies {
+            implementation(libs.compose.runtime)
+            implementation(libs.compose.foundation)
+            implementation(libs.compose.material3)
+            implementation(libs.compose.ui)
+            implementation(libs.compose.components.resources)
+            implementation(libs.compose.ui.tooling.preview)
+            implementation(libs.androidx.lifecycle.viewmodelCompose)
+            implementation(libs.androidx.lifecycle.runtimeCompose)
+
+            implementation(project.dependencies.platform(libs.koin.bom))
+            implementation(libs.bundles.koin.common)
+            implementation(libs.bundles.ktor.common)
+
+            implementation(libs.material3.adaptive)
+            implementation(libs.touchlab.kermit)
+            implementation(libs.kotlinx.serialization.json)
+            implementation(libs.jetbrains.compose.navigation)
+        }
+        commonTest.dependencies {
+            implementation(libs.kotlin.test)
+        }
+        iosMain.dependencies {
+            implementation(libs.ktor.client.darwin)
+        }
+    }
+}
+
+dependencies {
+    androidRuntimeClasspath(libs.compose.ui.tooling)
+}
---
+++ composeApp/src/commonMain/kotlin/com/mikitazayanchkouski/imageskmp/App.kt
@@ -0,0 +1,14 @@
+package com.mikitazayanchkouski.imageskmp
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview
+fun App() {
+    MaterialTheme {
+        Text("HELLO! :)")
+    }
+}
